35dfa09718fc4471cf8b73e1d04484fd2d6f9660
austin2118ace <austin2118ace@gmail.com>
2025-08-22 10:58:12 -0400

    Base Model: Default entries for the SettingsBase model

--- bpod_rig/config/models/base.py
+++ bpod_rig/config/models/base.py
@@ -1,10 +1,12 @@
 """Base classes for configuration classes"""
+import datetime
+from typing import Union
 from pydantic import BaseModel, PastDate
 
 class SettingsBase(BaseModel):
-    creation_date: PastDate
-    save_date: PastDate
-    author: str  # Username of settings creator
+    creation_date: Union[datetime.date, PastDate] = datetime.date.today()
+    save_date: Union[datetime.date, PastDate] = datetime.date(1970, 1, 1)
+    author: str  = None # Username of settings creator
 
 class ModuleBase(SettingsBase):
     name: str
