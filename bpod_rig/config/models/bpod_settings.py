56f5936870c5c2d1c05bc8a3ea266761a38a1bf2
austin2118ace <austin2118ace@gmail.com>
2025-08-22 10:58:51 -0400

    bpod_settings: Default Parameters so SystemSettings can be initialized with no input for testing

--- bpod_rig/config/models/bpod_settings.py
+++ bpod_rig/config/models/bpod_settings.py
@@ -1,7 +1,7 @@
 """Module implementing the base BpodSettings object"""
 
-from typing import Annotated, Optional, Union
-from pydantic import BaseModel, ConfigDict, DirectoryPath, PastDate
+from typing import Annotated, Optional
+from pydantic import ConfigDict, DirectoryPath, PastDate, UUID4
 from pydantic.types import StringConstraints, conbytes
 
 from config.models.base import SettingsBase
@@ -26,20 +26,24 @@ class BpodSettings(SettingsBase):
     behavior_ports: conbytes(max_length=1) # Can this be more than 8 bits?
 
 
-    protocol_dir: DirectoryPath
-    data_dir: DirectoryPath
-    calibration_dir: DirectoryPath
-    config_dir: DirectoryPath  # This might be redundant
 class SystemPaths(SettingsBase):
+    base_dir: DirectoryPath
+    protocol_dir: DirectoryPath = '.'
+    data_dir: DirectoryPath = '.'
+    calibration_dir: DirectoryPath = '.'
+    config_dir: DirectoryPath  = '.'  # This might be redundant
+    log_dir: DirectoryPath = '.'
 
 
-    current_version: str
-    last_update_check: Union[None, PastDate]
 class SystemSettings(SettingsBase):
+    current_version: str = '0.0.0'
+    last_update_check: PastDate = None
 
-    phone_home_id: Optional[str]
+    phone_home_id: Optional[UUID4] = None
     phone_home_opt_in: bool = False
 
-    bpods: list[BpodSettings]
-    paths: SystemPaths
+    debug: bool = False
+
+    bpods: list[BpodSettings] = []
+    paths: SystemPaths = SystemPaths()
     model_config = ConfigDict()
