8d47a1413e50af89a38422a1317a1a73c53e0814
austin2118ace <austin2118ace@gmail.com>
2025-09-01 18:57:52 -0400

    Bpod Settings Models: Formatting

--- bpod_rig/config/models/bpod_settings.py
+++ bpod_rig/config/models/bpod_settings.py
@@ -8,57 +8,61 @@ from config.models.base import SettingsBase
 class SystemPaths(SettingsBase):
     base_dir: Annotated[
         DirectoryPath,
-        Field(...,
+        Field(
+            ...,
             title="Local Bpod Directory",
             description="Local Bpod base directory where other folders are stored."
-                         " There is not any data directly stored in this directory",
-            examples = [r"C:\Users\BpodUser\Documents\Bpod",
-                        "/home/BpodUser/Documents/Bpod"]
-        )
+            " There is not any data directly stored in this directory",
+            examples=[
+                r"C:\Users\BpodUser\Documents\Bpod",
+                "/home/BpodUser/Documents/Bpod",
+            ],
+        ),
     ]
 
     protocol_dir: Annotated[
         DirectoryPath,
         Field(
-            '.',
+            ".",
             title="Bpod Protocol Directory",
             description="Local directory where Bpod protocols are stored."
-                        " The Protocol explorer will list all valid protocols found"
-                        " in this directory."
-        )
+            " The Protocol explorer will list all valid protocols found"
+            " in this directory.",
+        ),
     ]
 
     data_dir: Annotated[
         DirectoryPath,
         Field(
-            '.',
+            ".",
             title="Bpod Data Directory",
-            description="Local directory where data from protocol runs are stored."
-        )
+            description="Local directory where data from protocol runs are stored.",
+        ),
     ]
     calibration_dir: Annotated[
         DirectoryPath,
         Field(
-            '.',
+            ".",
             title="Bpod Calibration File Directory",
-            description="Local directory where Bpod water valve calibrations are stored."
-        )
+            description="Local directory where Bpod water"
+            " valve calibrations are stored.",
+        ),
     ]
     config_dir: Annotated[
         DirectoryPath,
         Field(
-            '.',
+            ".",
             title="Bpod Configuration Directory",
-            description="Local directory where Bpod configuration files are stored."
-        )
+            description="Local directory where Bpod configuration files are stored.",
+        ),
     ]
     log_dir: Annotated[
         DirectoryPath,
         Field(
-            '.',
+            ".",
             title="Bpod Log Directory",
-            description="Local directory where Bpod logs are stored."
-        )
+            description="Local directory where Bpod logs are stored.",
+        ),
     ]
 
 
@@ -66,10 +70,10 @@ class SystemSettings(SettingsBase):
     current_version: Annotated[
         str,
         Field(
-            '0.0.0',
+            "0.0.0",
             title="Current bpod-rig version",
-            description="Currently installed version of the bpod-rig repository"
-        )
+            description="Currently installed version of the bpod-rig repository",
+        ),
     ]
 
     last_update_check: Annotated[
@@ -77,8 +81,8 @@ class SystemSettings(SettingsBase):
         Field(
             title="Last update check date and time",
             description="Last date and time that system checked for any updates"
-                        " compared to the current version"
-        )
+            " compared to the current version",
+        ),
     ]
 
     phone_home_id: Annotated[
@@ -86,8 +90,8 @@ class SystemSettings(SettingsBase):
         Field(
             title="System's Unique Phone-Home ID",
             description="UUID4 ID of the current system"
-                        " for the Bpod phone-home telemetry."
-        )
+            " for the Bpod phone-home telemetry.",
+        ),
     ]
 
     phone_home_opt_in: Annotated[
@@ -96,8 +100,8 @@ class SystemSettings(SettingsBase):
             False,
             title="Phone-Home Opt-In",
             description="Whether or not the user has opted"
-                        " in to the phone-home telemetry."
-        )
+            " in to the phone-home telemetry.",
+        ),
     ]
 
     debug: Annotated[
@@ -105,15 +109,15 @@ class SystemSettings(SettingsBase):
         Field(
             False,
             title="Debug Enabled",
-            description="Set to True to enable debug information output."
-        )
+            description="Set to True to enable debug information output.",
+        ),
     ]
 
     paths: Annotated[
         SystemPaths,
         Field(
             title="System Paths Model",
-            description="Model containing validated bpod system paths"
-        )
+            description="Model containing validated bpod system paths",
+        ),
     ]
     model_config = ConfigDict()
