2ed79c808a002839a35a5343b8745c943cdc3591
austin2118ace <austin2118ace@gmail.com>
2025-09-01 18:41:13 -0400

    Annotate SystemPaths with Fields

--- bpod_rig/config/models/bpod_settings.py
+++ bpod_rig/config/models/bpod_settings.py
@@ -1,19 +1,65 @@
 """Module implementing the base BpodSettings object"""
 
 from typing import Annotated, Optional
-from pydantic import ConfigDict, DirectoryPath, PastDate, UUID4
-from pydantic.types import StringConstraints, conbytes
-
+from pydantic import ConfigDict, DirectoryPath, PastDate, UUID4, Field
 from config.models.base import SettingsBase
 
 
 class SystemPaths(SettingsBase):
-    base_dir: DirectoryPath
-    protocol_dir: DirectoryPath = '.'
-    data_dir: DirectoryPath = '.'
-    calibration_dir: DirectoryPath = '.'
-    config_dir: DirectoryPath  = '.'  # This might be redundant
-    log_dir: DirectoryPath = '.'
+    base_dir: Annotated[
+        DirectoryPath,
+        Field(...,
+            title="Local Bpod Directory",
+            description="Local Bpod base directory where other folders are stored."
+                         " There is not any data directly stored in this directory",
+            examples = [r"C:\Users\BpodUser\Documents\Bpod",
+                        "/home/BpodUser/Documents/Bpod"]
+        )
+    ]
+
+    protocol_dir: Annotated[
+        DirectoryPath,
+        Field(
+            '.',
+            title="Bpod Protocol Directory",
+            description="Local directory where Bpod protocols are stored."
+                        " The Protocol explorer will list all valid protocols found"
+                        " in this directory."
+        )
+    ]
+
+    data_dir: Annotated[
+        DirectoryPath,
+        Field(
+            '.',
+            title="Bpod Data Directory",
+            description="Local directory where data from protocol runs are stored."
+        )
+    ]
+    calibration_dir: Annotated[
+        DirectoryPath,
+        Field(
+            '.',
+            title="Bpod Calibration File Directory",
+            description="Local directory where Bpod water valve calibrations are stored."
+        )
+    ]
+    config_dir: Annotated[
+        DirectoryPath,
+        Field(
+            '.',
+            title="Bpod Configuration Directory",
+            description="Local directory where Bpod configuration files are stored."
+        )
+    ]
+    log_dir: Annotated[
+        DirectoryPath,
+        Field(
+            '.',
+            title="Bpod Log Directory",
+            description="Local directory where Bpod logs are stored."
+        )
+    ]
 
 
 class SystemSettings(SettingsBase):
