598d8f2edb64c477e728352d5ec3011f1e2013ef
austin2118ace <austin2118ace@gmail.com>
2025-09-01 18:57:40 -0400

    Base Model: Formatting

--- bpod_rig/config/models/base.py
+++ bpod_rig/config/models/base.py
@@ -1,12 +1,15 @@
 """Base classes for configuration classes"""
+
 import datetime
 from typing import Union
 from pydantic import BaseModel, PastDate
 
+
 class SettingsBase(BaseModel):
     creation_date: Union[datetime.date, PastDate] = datetime.date.today()
     save_date: Union[datetime.date, PastDate] = datetime.date(1970, 1, 1)
-    author: str  = None # Username of settings creator
+    author: str = None  # Username of settings creator
+
 
 class ModuleBase(SettingsBase):
     name: str
