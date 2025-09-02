99411314bf1e6ad51cd5ba75923428618addbcd3
austin2118ace <austin2118ace@gmail.com>
2025-09-01 20:01:50 -0400

    Initial_Configuration: boilerplate initial instantiation of configuration model

---
+++ bpod_rig/config/initial_configuration.py
@@ -0,0 +1,21 @@
+import datetime
+from pathlib import Path
+
+from bpod_rig.config.models.bpod_settings import SystemPaths, SystemSettings
+
+def init_system_configuration(bpod_dir: Path) -> SystemSettings:
+    system_paths = SystemPaths(
+        creation_date=datetime.date.today(),
+        base_dir=bpod_dir,
+        data_dir=bpod_dir.joinpath("Data"),
+        calibration_dir=bpod_dir.joinpath("Calibrations"),
+        config_dir=bpod_dir.joinpath("Settings"),
+        protocol_dir=bpod_dir.joinpath("Protocols")
+        # log_dir=bpod_dir.joinpath("Logs")
+    )
+
+    system_settings = SystemSettings(
+        paths=system_paths
+    )
+
+    return system_settings
