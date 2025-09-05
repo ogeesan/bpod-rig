07a52c9a3978ee05774359128c30e909a19834a4
austin2118ace <austin2118ace@gmail.com>
2025-09-04 22:09:47 -0400

    Main: import configuration modules; instantiate default configuration; save default configuration to disk This will include a check to load a preexisting settings file later

--- bpod_rig/__main__.py
+++ bpod_rig/__main__.py
@@ -2,7 +2,8 @@
 
 import logging
 
-from bpod_rig.IO import default_setup
+from bpod_rig.config import initial_configuration
+from bpod_rig.IO import default_setup, save_load_config
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -13,9 +14,15 @@ def main():
     # Setup folders. "EMU" is the USB serial port argument for the emulator.
     # create_default_directories() is called later to add folders for each FSM on the PC
     bpod_directory = default_setup.create_default_directories("EMU")
+
     # Populate this machine's folders with the default config and calibration files.
     default_setup.copy_default_files(bpod_directory, "EMU")
 
+    inital_system_config = initial_configuration.init_system_configuration(
+        bpod_directory
+    )
+
+    save_path = save_load_config.save_config_to_disk(inital_system_config)
 
 if __name__ == "__main__":
     main()
