d7c98ff995343768ea3ce9f01d8e925a567653e5
austin2118ace <austin2118ace@gmail.com>
2025-09-09 21:31:12 -0400

    BpodPaths: Add Pydantic model for BpodPaths which holds the config and calibration folders for a unique bpod machine
    SystemPaths: rename config_dir to base_config_dir

--- bpod_rig/config/models/bpod_settings.py
+++ bpod_rig/config/models/bpod_settings.py
@@ -1,8 +1,70 @@
 """Module implementing the base BpodSettings object"""
 
-from typing import Annotated, Optional
-from pydantic import ConfigDict, DirectoryPath, PastDate, UUID4, Field
+from typing import Annotated, Optional, Any
+from pydantic import ConfigDict, DirectoryPath, PastDate, UUID4, Field, field_validator
 from config.models.base import SettingsBase
+
+
+def bpod_path_factory(data: dict, addition: str):
+    """
+    Function to dynamically create BpodPaths subpaths at validation time.
+    Function creates a path in the below form:
+
+    {Parent_Dir}/Machine-{Bpod_ID}/[addition]
+
+    Path components in {} are retrieved from the dictionary of pre-validated data.
+
+    Parameters
+    ----------
+    data (dict): Dictionary containing all previously validated fields
+    addition (str): Addition to join to the end of the path
+
+    Returns
+    -------
+    (pathlib.Path): combined path in above form
+    """
+    return data["parent_dir"].joinpath(f"Machine-{data['bpod_id']}", addition)
+
+
+class BpodPaths(SettingsBase):
+    bpod_id: Annotated[
+        str,
+        Field(
+            ...,
+            title="Bpod ID",
+            description="Unique identifier for each Bpod connected to the system;"
+            " doubles as the name for the configuration sub directory name"
+            " for this Bpod",
+        ),
+    ]
+
+    parent_dir: Annotated[
+        DirectoryPath,
+        Field(
+            ...,
+            title="Bpod directory parent directory",
+            description="This is the parent directory to this unique Bpod's directory",
+        ),
+    ]
+
+    config_dir: Annotated[
+        Optional[DirectoryPath],
+        Field(
+            title="Bpod Configuration Subdirectory",
+            description="Sub directory where Bpod configuration files are "
+            "stored for this unique Bpod",
+            default_factory=lambda data: bpod_path_factory(data, "Config"),
+        ),
+    ]
+
+    calibration_dir: Annotated[
+        Optional[DirectoryPath],
+        Field(
+            default_factory=lambda data: bpod_path_factory(data, "Calibration"),
+            title="Bpod Configuration Directory",
+            description="Local directory where Bpod configuration files are stored.",
+        ),
+    ]
 
 
 class SystemPaths(SettingsBase):
@@ -40,7 +102,7 @@ class SystemPaths(SettingsBase):
         ),
     ]
 
-    config_dir: Annotated[
+    base_config_dir: Annotated[
         DirectoryPath,
         Field(
             ...,
@@ -48,6 +110,7 @@ class SystemPaths(SettingsBase):
             description="Local directory where Bpod configuration files are stored.",
         ),
     ]
+
     log_dir: Annotated[
         Optional[DirectoryPath],
         Field(
