841856845198f126435ce990c2f3dc128bf8f1c1
Austin Pauley <austin2118ace@gmail.com>
2025-09-12 20:31:44 -0400

    json_handler: read json function

--- bpod_rig/IO/json_handler.py
+++ bpod_rig/IO/json_handler.py
@@ -42,10 +42,39 @@ def write_json(
                 logger.debug("Saving JSON to %s", str(full_save_path))
                 json_stream.write(json_content)
     except (IOError, OSError) as e:
-        logger.error("Error opening or writing file to disk!", exc_info=e)
+        logger.error("Error writing file to disk!", exc_info=e)
 
     return full_save_path
 
+
+def read_json(
+    file_path: Path
+) -> Union[str, None]:
+    """
+    Read json-formatted data from disk.
+
+    Parameters
+    ----------
+    file_path: pathlib.Path
+        Path with filename and extension of file to read
+
+    Returns
+    -------
+    str
+        Json-formatted data read from provided file path
+    """
+
+
+    if not file_path.exists():
+        logger.error("File [%s] does not exist!", file_path)
+        return None
+
+    try:
+        with open(file_path, "r") as json_stream:
+            logger.debug("Reading JSON from %s", str(file_path))
+            return json_stream.read()
+    except (IOError, OSError) as e:
+        logger.error("Error opening and reading file!", exc_info=e)
 
 
 
