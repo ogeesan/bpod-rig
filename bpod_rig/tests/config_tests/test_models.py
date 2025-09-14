99b96084531ea1faa902e72133f9214aaec34b1d
austin2118ace <austin2118ace@gmail.com>
2025-09-14 14:04:50 -0400

    TestSystemPathsModel: unit testing for the SystemPaths model

--- bpod_rig/tests/config_tests/test_models.py
+++ bpod_rig/tests/config_tests/test_models.py
@@ -1,9 +1,13 @@
 import datetime
+import pathlib
+import shutil
+import tempfile
 import unittest
 
 from pydantic import ValidationError
 
 from bpod_rig.config.models.base import SettingsBase
+from bpod_rig.config.bpod_system import SystemPaths
 
 class TestBaseModel(unittest.TestCase):
     def test_default(self): # NOQA N802
@@ -41,7 +45,66 @@ class TestBaseModel(unittest.TestCase):
             too_short_username = ""
             SettingsBase(username=too_short_username)
 
+class TestSystemPathsModel(unittest.TestCase):
+    def setUp(self):
+        self.working_dir = pathlib.Path(tempfile.mkdtemp())
+        self.bpod_dir = self.working_dir.joinpath('Bpod')
+        self.config_dir = self.working_dir.joinpath('Config')
+        self.protocol_dir = self.working_dir.joinpath('Protocols')
+        self.data_dir = self.working_dir.joinpath('Data')
+        # self.log_dir = self.working_dir.joinpath('Logs')
+        # TODO: enable this once we add log dir
 
+    def test_default_factory(self):
+        try:
+            sp = SystemPaths(base_dir=self.bpod_dir)
+            self.assertEqual(sp.data_dir, self.data_dir)
+            self.assertEqual(sp.base_config_dir, self.config_dir)
+            self.assertEqual(sp.protocol_dir, self.protocol_dir)
+            # self.assertEqual(sp.log_dir, self.log_dir)
+        except ValidationError:
+            # This will raise a validation error, but I just want to test the
+            # default factory in isolation
+            pass
+
+    def test_non_exists(self):
+        with self.assertRaises(ValidationError):
+            # Bpod dir does not exist; raises validation error
+            SystemPaths(base_dir=self.bpod_dir)
+        with self.assertRaises(ValidationError):
+            # Bpod dir exists now; other dirs do not
+            self.bpod_dir.mkdir(exist_ok=True)
+            SystemPaths(base_dir=self.bpod_dir)
+        with self.assertRaises(ValidationError):
+            # Bpod dir and data dir exist
+            self.data_dir.mkdir(exist_ok=True)
+            SystemPaths(
+                base_dir=self.bpod_dir,
+                data_dir=self.data_dir
+            )
+        with self.assertRaises(ValidationError):
+            # Bpod dir, data, and protocol exists
+            self.protocol_dir.mkdir(exist_ok=True)
+            SystemPaths(
+                base_dir=self.bpod_dir,
+                data_dir=self.data_dir,
+                protocol_dir=self.protocol_dir
+            )
+        with self.assertRaises(ValidationError):
+            # Bpod dir, data, and config exist; delete protocol
+            self.protocol_dir.rmdir()
+            self.config_dir.mkdir(exist_ok=True)
+            SystemPaths(
+                base_dir=self.bpod_dir,
+                data_dir=self.data_dir,
+                base_config_dir=self.config_dir,
+            )
+
+
+
+    def tearDown(self):
+        # Since we are not using a context manager we gotta clean this up on our own
+        shutil.rmtree(self.working_dir)
 
 if __name__ == "__main__":
     unittest.main()
