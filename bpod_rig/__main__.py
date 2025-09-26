63730dbb5d7a5d56ecfb85975cd7151155b2b1e3
Austin Pauley <austin2118ace@gmail.com>
2025-09-26 13:09:53 -0400

    Fix missing import

--- bpod_rig/__main__.py
+++ bpod_rig/__main__.py
@@ -2,14 +2,16 @@
 
 import logging
 
-from bpod_rig.config import initial_configuration
-from bpod_rig.IO import default_setup, json_handler
+from bpod_rig.config import utils
+from bpod_rig.IO import default_setup
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 def main():
+    ### Everything below is subject to change and is for testing purposes only
+
     logger.info("Initializing bpod-rig!")
     # Setup folders. "EMU" is the USB serial port argument for the emulator.
     # create_default_directories() is called later to add folders for each FSM on the PC
@@ -18,9 +20,11 @@ def main():
     # Populate this machine's folders with the default config and calibration files.
     default_setup.copy_default_files(bpod_directory, "EMU")
 
-    inital_system_config = initial_configuration.init_system_configuration(
+    inital_system_config = utils.init_system_configuration(
         bpod_directory
     )
+
+    result = utils.save_system_configuration(inital_system_config)
 
 if __name__ == "__main__":
     main()
